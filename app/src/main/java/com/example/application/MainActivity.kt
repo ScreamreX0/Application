d25a485a35365879b35e7312f95db902778eb413
scx <scx0503@gmail.com>
2023-01-13 15:32:47 +0300

    merge

--- app/src/main/java/com/example/application/MainActivity.kt
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // feature-signin -> ui elements
-        // test-signin-ui -> added tests
+        // test-signin-ui -> added tests, merge
     }
 }
